432f66363ba7b0b3a40a292dcae5ca839fcafb12
Artem <aidar741@gmail.com>
2023-06-24 00:20:35 +0300

    Update2.0

--- src/main/java/org/example/Handler.java
+++ src/main/java/org/example/Handler.java
@@ -1,5 +1,8 @@
 package org.example;
 
-interface Handler{
+import java.io.BufferedOutputStream;
 
+@FunctionalInterface
+public interface Handler {
+    void handle(Request request, BufferedOutputStream out);
 }
